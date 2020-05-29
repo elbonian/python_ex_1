9bd4d439284d38c82c060cec1af1c4920f079e92
Marcel <elbonian@gmail.com>
2020-05-29 09:34:31 -0700

    Finished part 2

--- refresher_ex_1.py
+++ refresher_ex_1.py
@@ -166,7 +166,16 @@ print(newL) # [0, 2, 3, [5, 6], 8, 10]
 print("start of part 2") # set breakpoint here
 s = "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects."
 # your code here
-
+# We use the split() function to break a string into a list of items separated by the parameter
+# Note that we use ". " as this removes the space leading into sentences after the first one
+sentences = s.split(". ")
+# As required, we will count sentences. We are using i as the counter.
+i = 0
+for x in sentences:
+  # We increment the sentence counter
+  i=i+1
+  # And now we pretty-print the sentence
+  print("Sentence " + str(i) + " is: " + x.replace('.', ''))
 print("end of 2") # set breakpoint here 
 '''
 
